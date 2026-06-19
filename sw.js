6568759a8e5c37f502b2593181aa407eeee0ec9a
ccp2002 <2979852949@qq.com>
2026-06-19 12:10:00 +0800

    add sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+﻿const CACHE_NAME = "fitness-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
